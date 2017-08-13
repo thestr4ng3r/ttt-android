a87a3d3ca99cf64f6c7fd3dc979f10d29c856c55
Florian Märkl <info@florianmaerkl.de>
2017-08-13 18:56:44 +0200

    Main Activity Layout

--- app/src/main/java/com/metallic/tttandroid/MainActivity.kt
+++ app/src/main/java/com/metallic/tttandroid/MainActivity.kt
@@ -2,12 +2,17 @@ package com.metallic.tttandroid
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : Activity()
+class MainActivity : AppCompatActivity()
 {
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		setSupportActionBar(toolbar)
+		title = getString(R.string.activity_main_title)
 	}
 }
--- app/src/main/java/com/metallic/tttandroid/model/AppDatabase.kt
+++ app/src/main/java/com/metallic/tttandroid/model/AppDatabase.kt
@@ -1,9 +1,11 @@
 package com.metallic.tttandroid.model
 
+import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
 
+@Database(entities = arrayOf(Feed::class), version = 1)
 abstract class AppDatabase: RoomDatabase()
 {
 	abstract fun feedDao(): FeedDao
