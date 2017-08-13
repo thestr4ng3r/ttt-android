85d5e8d7404daa400ebad9e15a30224fd0b23789
Florian Märkl <info@florianmaerkl.de>
2017-08-13 15:00:16 +0200

    Initial Commit

---
+++ app/src/main/java/com/metallic/tttandroid/model/FeedDao.kt
@@ -0,0 +1,5 @@
+package com.metallic.tttandroid.model
+
+/**
+ * Created by florian on 13.08.17.
+ */
---
+++ app/src/main/kotlin/com/metallic/tttandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.metallic.tttandroid
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
