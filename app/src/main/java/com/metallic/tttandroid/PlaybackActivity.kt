6f08e9c1b51c583b13616207ce03b3dda0b7c574
Florian Märkl <info@florianmaerkl.de>
2017-08-25 22:23:52 +0200

    Playback Controls hhiding

--- app/src/main/java/com/metallic/tttandroid/PlaybackActivity.kt
+++ app/src/main/java/com/metallic/tttandroid/PlaybackActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.SeekBar
@@ -26,6 +27,8 @@ class PlaybackActivity: LifecycleAppCompatActivity(), Recording.Listener
 	private lateinit var viewModel: PlaybackViewModel
 
 	private lateinit var imageView: ImageView
+
+	private lateinit var playbackControlsView: View
 	private lateinit var seekBar: SeekBar
 	private lateinit var playButton: ImageButton
 	private lateinit var durationTextView: TextView
@@ -35,10 +38,17 @@ class PlaybackActivity: LifecycleAppCompatActivity(), Recording.Listener
 
 	private var currentPositionSeconds = 0
 
+	private var playbackControlsDisplayed = true
+	private lateinit var hidePlaybackControlsHandler: Handler
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_playback)
+
+		window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+				View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 
 		val feedId = intent.getLongExtra(EXTRA_FEED_ID, -1)
 		val feedItemTitle = intent.getStringExtra(EXTRA_FEED_ITEM_TITLE)
@@ -58,6 +68,8 @@ class PlaybackActivity: LifecycleAppCompatActivity(), Recording.Listener
 			imageView.invalidate()
 		})
 
+		playbackControlsView = playback_controls_view
+
 		seekBar = progress_seek_bar
 		seekBar.max = viewModel.recording!!.duration
 		seekBar.setOnSeekBarChangeListener(seekBarListener)
@@ -74,12 +86,57 @@ class PlaybackActivity: LifecycleAppCompatActivity(), Recording.Listener
 		updatePlayButton(viewModel.recording?.isPlaying)
 		updatePositionTextView(viewModel.recording?.time)
 		updateDurationTextView(viewModel.recording?.duration)
+
+		hidePlaybackControlsHandler = Handler()
+
+		imageView.setOnClickListener { animatePlaybackControls() }
+		playbackControlsView.setOnClickListener { animatePlaybackControls(true) }
+
+		animatePlaybackControls(true)
+	}
+
+	private fun animatePlaybackControls(show: Boolean = !playbackControlsDisplayed)
+	{
+		val fullDuration = resources.getInteger(R.integer.playback_controls_anim_duration).toLong()
+		val targetAlpha = if(show) 1.0f else 0.0f
+		val currentAlpha = playbackControlsView.alpha
+		val duration = (fullDuration * Math.abs(targetAlpha - currentAlpha)).toLong()
+
+		if(duration > 0L)
+		{
+			playbackControlsView.animate()
+					.alpha(targetAlpha)
+					.setDuration(duration)
+					.start()
+
+			playbackControlsDisplayed = show
+		}
+
+		if(playbackControlsDisplayed)
+			schedulePlaybackControlsHiding()
+	}
+
+	private val hidePlaybackControlsCallback = Runnable {
+		if(viewModel.recording?.isPlaying ?: true)
+			animatePlaybackControls(false)
+	}
+
+	private fun schedulePlaybackControlsHiding(clearOnly: Boolean = false)
+	{
+		hidePlaybackControlsHandler.removeCallbacks(hidePlaybackControlsCallback)
+
+		if(clearOnly)
+			return
+
+		val timeout = resources.getInteger(R.integer.playback_controls_hide_timeout)
+		hidePlaybackControlsHandler.postDelayed(hidePlaybackControlsCallback, timeout.toLong())
 	}
 
 	override fun onDestroy()
 	{
 		super.onDestroy()
 		viewModel.recording?.removeListener(this)
+		schedulePlaybackControlsHiding(true)
 	}
 
 	private fun updatePlayButton(playing: Boolean?)
@@ -149,6 +206,8 @@ class PlaybackActivity: LifecycleAppCompatActivity(), Recording.Listener
 		updateHandler.post {
 			updatePlayButton(playing)
 		}
+
+		schedulePlaybackControlsHiding(!playing)
 	}
 
 	override fun timeChanged(recording: Recording, time: Int)
